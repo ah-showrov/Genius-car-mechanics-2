d139e6bb60d5d688ab07cc06ea7cd042e9f59ed6
Showrov <ahshowrov1@gmail.com>
2021-10-24 10:45:20 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home/Home";
+import NotFound from "./Pages/NotFound/NotFound";
+import Header from "./Pages/Shared/Header/Header";
+import Login from "./Pages/Login/Login";
+import AuthProvider from "./context/AuthProvider";
+import Booking from "./Booking/Booking/Booking";
+import PrivateRoute from "./Pages/PrivateRoute/PrivateRoute";
+
+function App() {
+  return (
+    <div className="App">
+      <AuthProvider>
+        <BrowserRouter>
+          <Header></Header>
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route path="/home">
+              <Home></Home>
+            </Route>
+            <PrivateRoute path="/booking/:serviceId">
+              <Booking></Booking>
+            </PrivateRoute>
+            <Route path="/login">
+              <Login></Login>
+            </Route>
+            <Route path="*">
+              <NotFound></NotFound>
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </AuthProvider>
+    </div>
+  );
+}
+
+export default App;
